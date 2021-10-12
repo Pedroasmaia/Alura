6a4f0c757f0c64fa4f8db9c99aee27aa02797c4d
Pedro Augusto <pedroasmaia@outlook.com>
2021-10-12 00:34:33 -0300

    Iterando de maneira diferentes e finalizando Loops

--- Python/Adivinhação_for.py
+++ Python/Adivinhação_for.py
@@ -5,13 +5,16 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada,total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite o seu número entre 1 e 100: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -25,5 +28,4 @@ while(rodada <= total_de_tentativas):
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
-    rodada = rodada + 1
 print("Fim do jogo")
